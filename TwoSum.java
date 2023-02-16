cef7bf20bef785a679e5dc66d9cda5871a9d2ee9
vinothnda <vinonada@gmail.com>
2023-02-16 22:20:33 +0530

    412 fizzbuzz

---
+++ TwoSum.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] question = {6, 2, 3};
+        int[] arr = twoSum(question, 9);
+        System.out.println(Arrays.toString(arr));
+        ;
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j && nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
