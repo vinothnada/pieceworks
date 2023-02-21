b285ddd250b0f8d5c13c9243ddcbb000b603fd58
vinothnda <vinonada@gmail.com>
2023-02-21 20:52:23 +0530

    findSumDigits function

---
+++ udemy/SumOfDigits.java
@@ -0,0 +1,26 @@
+package udemy;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+//        System.out.println(findSumDigits(152));
+        System.out.println(findSumDigits2(253));
+    }
+
+    static int findSumDigits(int num){
+        int sum = 0;
+        int val = num/10;
+        int rem = num%10;
+        if(val > 10){
+            int val2 = findSumDigits(val);
+            sum += val2;
+        }else{
+            sum += val;
+        }
+        sum += rem;
+        return sum;
+    }
+
+    static int findSumDigits2(int num){
+        return num%10 + findSumDigits(num/10);
+    }
+}
